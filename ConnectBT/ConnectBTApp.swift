5eecab65955c586173b5a612c8be8eabb1e1bb27
Sarah Jeong <pishon313@gmail.com>
2024-05-14 15:56:21 +0900

    Initial Commit

---
+++ ConnectBT/ConnectBTApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConnectBTApp.swift
+//  ConnectBT
+//
+//  Created by tsis-Macbook Pro on 5/14/24.
+//
+
+import SwiftUI
+
+@main
+struct ConnectBTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
